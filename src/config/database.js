85ad965ccc0bdcd62d7be8ad5043f26519d1c0d7
aleep98 <zero167@outlook.com>
2026-06-15 14:37:14 -0300

    feat: refatorar função de conexão com o banco de dados para incluir tratamento de erros

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 
 async function connectDatabase() {
-    mongoose.connect(process.env.MONGO_URI)
 
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECTION)
+        console.log('Conectado ao banco de dados')
+    } catch (error) {
+        console.log('Erro ao conectar ao banco de dados', error)
+    }
     return mongoose.connection;
-
 }
 
 export default connectDatabase;
